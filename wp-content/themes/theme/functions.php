9c34e049701b602803c2e2cd5fccdf1849e83f88
Yurii Vlasenko <vlasenkoj@gmail.com>
2016-06-06 01:34:31 +0300

    Added wordpress

--- wp-content/themes/theme/functions.php
+++ wp-content/themes/theme/functions.php
@@ -37,14 +37,14 @@ function vlas_pagenavi() {
     $a['total'] = $max;
     $a['current'] = $current;
 
-    $total = 1; //1 - выводить текст "Страница N из N", 0 - не выводить
+    $total = 0; //1 - выводить текст "Страница N из N", 0 - не выводить
     $a['mid_size'] = 3; //сколько ссылок показывать слева и справа от текущей
     $a['end_size'] = 1; //сколько ссылок показывать в начале и в конце
     $a['prev_text'] = ''; //текст ссылки "Предыдущая страница"
     $a['next_text'] = ''; //текст ссылки "Следующая страница"
 
     if ($max > 1) echo '<div class="animated fadeInUp go navigation">';
-    //if ($total == 1 && $max > 1) $pages = '<span class="pages">Страница ' . $current . ' из ' . $max . '</span>'."\r\n";
+    if ($total == 1 && $max > 1) $pages = '<span class="pages">Страница ' . $current . ' из ' . $max . '</span>'."\r\n";
     echo $pages . paginate_links($a);
     if ($max > 1) echo '</div>';
 }
